374741d44c662d8b4729407c03f7b285e06343cd
Bernardo <bernafernandeztx@gmail.com>
2024-07-15 23:13:56 -0300

    add name variable and age variable

--- main.java
+++ main.java
@@ -1,4 +1,11 @@
-public class main { public static void main(String[] args) {
-        
+package variables;
+public class main {
+    public static void main(String[] args) {
+      String firstName = "Bernardo ";
+      String lastName = "Fernandez";
+      String fullName = firstName + lastName;
+      System.out.println(fullName);  
+      int idade = 15;
+      System.out.println(idade);
     }
-}
+  }
